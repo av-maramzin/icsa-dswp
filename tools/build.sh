04911e86f39186811eb7fe4ec8441a2e8a697f1b
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2015-11-19 10:06:46 +0000

    add LLVM and build specific conf to cmake call

--- tools/build.sh
+++ tools/build.sh
@@ -10,7 +10,21 @@ echo "project root dir: ${PROJECT_ROOT}"
 mkdir -p build
 cd build
 
+
+# notes on cmake call
+
+#- CC/CXX env variables dictate the compiler cmake should use
+#- LLVM_DIR provides a hint for cmake's find_package for LLVM
+#- CMAKE_EXPORT_COMPILE_COMMANDS exports the project's compilation db
+#  (e.g. use with YCM)
+
 CC=clang \
-cmake ${PROJECT_ROOT}
+cmake \
+  -DLLVM_DIR=$(llvm-config --prefix)/share/llvm/cmake/ \
+  -DCMAKE_BUILD_TYPE=DEBUG \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DCMAKE_INSTALL_PREFIX=../install \
+  ${PROJECT_ROOT}
+
 make
 
