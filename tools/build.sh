a248ad0ff344971189fda2fbc3e5e319e6c28b36
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2015-11-19 09:57:41 +0000

    fix build script to work from any location
    As long as it does not get sourced, you can build the project from any
    location in the filesystem. Unfortunately, it is not safe to add the
    same functinality to the corresponding clean script

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
+
+SELF_DIR=$(dirname $0)
+PROJECT_ROOT=${PWD}/${SELF_DIR}/../
+
+#
+
+echo "project root dir: ${PROJECT_ROOT}"
+
 mkdir -p build
 cd build
-cmake ..
+
+CC=clang \
+cmake ${PROJECT_ROOT}
 make
+
