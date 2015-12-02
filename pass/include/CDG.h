6989bd21da90a4e818f3ebb7071e0f3c62a56760
Stan Manilov <stanislav.manilov@gmail.com>
2015-12-02 18:31:50 +0000

    Abstract dependence graphs away

--- pass/include/CDG.h
+++ pass/include/CDG.h
@@ -1,117 +1,21 @@
-// Implemented in DSWPPass.cpp.
 #ifndef ICSA_DSWP_CDG_H
 #define ICSA_DSWP_CDG_H
 
-#include <memory>
-using std::shared_ptr;
-#include <vector>
-using std::vector;
-#include <map>
-using std::map;
-#include <string>
-using std::string;
-
 #include "llvm/Support/raw_ostream.h"
 using llvm::raw_ostream;
-
-#include "llvm/ADT/SmallVector.h"
-using llvm::SmallVectorImpl;
-#include "llvm/ADT/SmallPtrSet.h"
-using llvm::SmallPtrSet;
-
-#include "llvm/IR/Module.h"
-using llvm::Module;
-#include "llvm/IR/Function.h"
-using llvm::Function;
 #include "llvm/IR/BasicBlock.h"
 using llvm::BasicBlock;
 
-#include "llvm/Pass.h"
-using llvm::FunctionPass;
-#include "llvm/PassAnalysisSupport.h"
-using llvm::AnalysisUsage;
-
-#include "llvm/Analysis/PostDominators.h"
-using llvm::PostDominatorTree;
+#include "Dependence.h"
 
 namespace icsa {
 
-// Designed after DomTreeNodeBase in llvm/Support/GenericDomTree.h.
-class ControlDependenceNode {
-  BasicBlock *TheBB;
-  vector<ControlDependenceNode *> Children;
+typedef Dependence<BasicBlock> ControlDependenceNode;
 
-public:
-  typedef typename vector<ControlDependenceNode *>::iterator iterator;
-  typedef
-      typename vector<ControlDependenceNode *>::const_iterator const_iterator;
-
-  iterator begin() { return Children.begin(); }
-  iterator end() { return Children.end(); }
-  const_iterator begin() const { return Children.begin(); }
-  const_iterator end() const { return Children.end(); }
-
-  BasicBlock *getBlock() const { return TheBB; }
-  const vector<ControlDependenceNode *> &getChildren() const {
-    return Children;
-  }
-
-  ControlDependenceNode(BasicBlock *BB) : TheBB(BB) {}
-
-  void addChild(shared_ptr<ControlDependenceNode> C) {
-    Children.push_back(C.get());
-  }
-
-  size_t getNumChildren() const { return Children.size(); }
-
-  void clearAllChildren() { Children.clear(); }
-
-  /// Return true if the nodes are not the same and false if they are the same.
-  bool compare(const ControlDependenceNode *Other) const {
-    if (getNumChildren() != Other->getNumChildren()) {
-      return true;
-    }
-
-    SmallPtrSet<const BasicBlock *, 4> OtherChildren;
-    for (const_iterator I = Other->begin(), E = Other->end(); I != E; ++I) {
-      const BasicBlock *BB = (*I)->getBlock();
-      OtherChildren.insert(BB);
-    }
-
-    for (const_iterator I = begin(), E = end(); I != E; ++I) {
-      const BasicBlock *BB = (*I)->getBlock();
-      if (OtherChildren.count(BB) == 0) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  void print(raw_ostream &OS) const {
-    getBlock()->printAsOperand(OS, false);
-    OS << ":";
-    for (const_iterator it = begin(); it != end(); ++it) {
-      if (it != begin()) {
-        OS << ",";
-      }
-      OS << " ";
-      (*it)->getBlock()->printAsOperand(OS, false);
-    }
-  }
-};
-
-// Designed after the PostDominatorTree struct in
-// llvm/Analysis/PostDominators.h.
-class ControlDependenceGraph : public FunctionPass {
-private:
-  typedef map<BasicBlock *, shared_ptr<ControlDependenceNode>> CDGNodeMapType;
-  CDGNodeMapType CDGNodes;
-  BasicBlock *firstBB;
-
+class ControlDependenceGraph : public DependenceGraph<BasicBlock> {
 public:
   static char ID;
-
-  ControlDependenceGraph() : FunctionPass(ID) {}
+  ControlDependenceGraph() : DependenceGraph<BasicBlock>(ID) {}
 
   bool runOnFunction(Function &F) override;
 
@@ -119,93 +23,17 @@ public:
     Info.addRequired<PostDominatorTree>();
   }
 
-  const Function *getFunction() const { return firstBB->getParent(); }
-
-  ControlDependenceNode *getRootNode() const { return getNode(firstBB); }
-
-  unsigned getNumNodes() const { return CDGNodes.size(); }
-
-  ControlDependenceNode *operator[](BasicBlock *BB) const;
-
-  ControlDependenceNode *getNode(BasicBlock *BB) const;
-
-  bool dependsOn(ControlDependenceNode *A, ControlDependenceNode *B) const;
-
-  bool dependsOn(BasicBlock *A, BasicBlock *B) const;
-
-  /// Get all nodes that have a control dependence on R.
-  void getDependants(BasicBlock *R,
-                     SmallVectorImpl<BasicBlock *> &Result) const;
+  const Function *getFunction() const { return firstValue->getParent(); }
 
   void releaseMemory() override;
 
   void print(raw_ostream &OS, const Module *) const override;
-
-  template <typename ValueType> friend class CDGBaseIterator;
 };
 
-// Iterator for all CDG nodes.
-
-template <typename ValueType>
-class CDGBaseIterator
-    : public std::iterator<std::bidirectional_iterator_tag, ValueType, int,
-                           ValueType *, ValueType *> {
-  typedef std::iterator<std::bidirectional_iterator_tag, ValueType, int,
-                        ValueType *, ValueType *> super;
-
-public:
-  typedef typename super::pointer pointer;
-  typedef typename super::reference reference;
-  typedef ControlDependenceGraph::CDGNodeMapType::const_iterator IteratorType;
-  typedef CDGBaseIterator<ValueType> thisType;
-
-private:
-  const ControlDependenceGraph &Graph;
-  IteratorType idx;
-
-public:
-  explicit inline CDGBaseIterator<ValueType>(const ControlDependenceGraph &G)
-      : Graph(G), idx(G.CDGNodes.begin()) {}
-
-  inline CDGBaseIterator<ValueType>(const ControlDependenceGraph &G, bool)
-      : Graph(G), idx(G.CDGNodes.end()) {}
-
-  inline bool operator==(const thisType &other) const {
-    return idx == other.idx;
-  }
-
-  inline bool operator!=(const thisType &other) const {
-    return !operator==(other);
-  }
-
-  inline reference operator*() const { return idx->second.get(); }
-  inline pointer operator->() const { return operator*(); }
-
-  inline thisType &operator++() {
-    ++idx;
-    return *this;
-  }
-
-  inline thisType operator++(int) {
-    thisType tmp = *this;
-    ++*this;
-    return tmp;
-  }
-
-  inline thisType &operator--() {
-    --idx;
-    return *this;
-  }
-
-  inline thisType operator--(int) {
-    thisType tmp = *this;
-    --*this;
-    return tmp;
-  }
-};
-
-typedef CDGBaseIterator<ControlDependenceNode> cdg_iterator;
-typedef CDGBaseIterator<const ControlDependenceNode> cdg_const_iterator;
+typedef DependenceGraphBaseIterator<BasicBlock, ControlDependenceNode>
+    cdg_iterator;
+typedef DependenceGraphBaseIterator<BasicBlock, const ControlDependenceNode>
+    cdg_const_iterator;
 }
 
 #endif
--- pass/src/CDG.cpp
+++ pass/src/CDG.cpp
@@ -43,7 +43,7 @@ RegisterPass<ControlDependenceGraph>
     CDGRegister("cdg", "Build Control Dependence Graph");
 
 bool ControlDependenceGraph::runOnFunction(Function &F) {
-  firstBB = &F.getEntryBlock();
+  firstValue = &F.getEntryBlock();
 
   PostDominatorTree &pdt = Pass::getAnalysis<PostDominatorTree>();
 
@@ -72,7 +72,7 @@ bool ControlDependenceGraph::runOnFunction(Function &F) {
     top_down_traversal.pop_back();
 
     BasicBlock *currentBB = current->getBlock();
-    CDGNodes[currentBB] =
+    Nodes[currentBB] =
         shared_ptr<ControlDependenceNode>(new ControlDependenceNode(currentBB));
 
     // TODO(Stan): skip dominance_frontier and directly build CDG
@@ -100,10 +100,10 @@ bool ControlDependenceGraph::runOnFunction(Function &F) {
   // Reverse the dominance_frontier map and store as a graph.
   for (auto &kv : dominance_frontier) {
     BasicBlock *to = kv.first;
-    CDGNodeMapType::iterator to_it = CDGNodes.find(to);
+    NodeMapType::iterator to_it = Nodes.find(to);
 
     for (BasicBlock *from : *kv.second) {
-      CDGNodeMapType::iterator from_it = CDGNodes.find(from);
+      NodeMapType::iterator from_it = Nodes.find(from);
 
       auto from_node = from_it->second.get();
       auto to_node = to_it->second;
@@ -114,53 +114,9 @@ bool ControlDependenceGraph::runOnFunction(Function &F) {
   return true;
 }
 
-ControlDependenceNode *ControlDependenceGraph::
-operator[](BasicBlock *BB) const {
-  return getNode(BB);
-}
-
-ControlDependenceNode *ControlDependenceGraph::getNode(BasicBlock *BB) const {
-  CDGNodeMapType::const_iterator I = CDGNodes.find(BB);
-  if (I != CDGNodes.end())
-    return I->second.get();
-  return nullptr;
-}
-
-bool ControlDependenceGraph::dependsOn(ControlDependenceNode *A,
-                                       ControlDependenceNode *B) const {
-  for (ControlDependenceNode::const_iterator it = B->begin(); it != B->end();
-       ++it) {
-    if (false == (*it)->compare(A))
-      return true;
-  }
-
-  for (ControlDependenceNode::const_iterator it = B->begin(); it != B->end();
-       ++it) {
-    if (dependsOn(A, (*it)))
-      return true;
-  }
-
-  return false;
-}
-
-bool ControlDependenceGraph::dependsOn(BasicBlock *A, BasicBlock *B) const {
-  ControlDependenceNode *AN = getNode(A), *BN = getNode(B);
-  return dependsOn(AN, BN);
-}
-
-void ControlDependenceGraph::getDependants(
-    BasicBlock *R, SmallVectorImpl<BasicBlock *> &Result) const {
-  Result.clear();
-  const ControlDependenceNode *RN = getNode(R);
-  for (ControlDependenceNode::const_iterator it = RN->begin(); it != RN->end();
-       ++it) {
-    Result.push_back((*it)->getBlock());
-  }
-}
-
 void ControlDependenceGraph::releaseMemory() {
-  firstBB = nullptr;
-  CDGNodes.clear();
+  firstValue = nullptr;
+  Nodes.clear();
 }
 
 void ControlDependenceGraph::print(raw_ostream &OS, const Module *) const {
@@ -168,7 +124,7 @@ void ControlDependenceGraph::print(raw_ostream &OS, const Module *) const {
   OS << "Control Dependence Graph: ";
   OS << "<node: dependants>";
   OS << "\n";
-  for (CDGNodeMapType::const_iterator I = CDGNodes.begin(); I != CDGNodes.end();
+  for (NodeMapType::const_iterator I = Nodes.begin(); I != Nodes.end();
        ++I) {
     I->second.get()->print(OS);
     OS << '\n';
